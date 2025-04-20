1c7ee1a444e7d2be7ec57ab1bcda0b1f917cea07
fijiro <jiro.protonmail@protonmail.com>
2025-04-20 15:05:08 +0300

    Rework gun building more scalable

--- scripts/workbench.gd
+++ scripts/workbench.gd
@@ -1,5 +1,4 @@
 extends InteractableObject
-
 func _ready():
 	super._ready()
 	# Hook up close callback and buttons
@@ -8,22 +7,21 @@ func _ready():
 
 #TODO:
 func _build_weapon():
-	var grip = (inventory.get_ui_slot(0) as DraggableItem).item
-	var receiver = (inventory.get_ui_slot(1) as DraggableItem).item
-	var barrel = (inventory.get_ui_slot(2) as DraggableItem).item
+	var parts: Dictionary = (inventory as WorkbenchInventory).get_parts()
+	print(parts)
+	var grip: Node3D = parts["grip"] if parts.has("grip") else null
+	var receiver: Node3D = parts["receiver"] if parts.has("receiver") else null
+	var barrel: Node3D = parts["barrel"] if parts.has("barrel") else null
+	var stock: Node3D = parts["stock"] if parts.has("stock") else null
 	if null in [grip, receiver, barrel]: return
-	else: print("OMG BUILD POSSIBLE HOLY SHIT")
+	#else: print("BUILD POSSIBLE")
 	grip.visible = true
-	grip.global_position = (get_node("GripPosition") as Marker3D).global_position
-	grip.rotation = (get_node("GripPosition") as Marker3D).rotation
 	receiver.visible = true
-	receiver.global_position = (get_node("ReceiverPosition") as Marker3D).global_position
-	receiver.rotation = (get_node("ReceiverPosition") as Marker3D).rotation
 	barrel.visible = true
-	barrel.global_position = (get_node("BarrelPosition") as Marker3D).global_position
-	barrel.rotation = (get_node("BarrelPosition") as Marker3D).rotation
+	stock.visible = true
 	grip.reparent(receiver, false)
 	barrel.reparent(receiver, false)
+	stock.reparent(receiver, false)
 	# Calculate correct positions for part
 	var barrel_to_receiver = barrel.find_child("Receiver_Attach")
 	var receiver_to_barrel = receiver.find_child("Barrel_Attach")
@@ -31,17 +29,25 @@ func _build_weapon():
 	var grip_to_receiver = grip.find_child("Receiver_Attach")
 	var receiver_to_grip = receiver.find_child("Grip_Attach")
 	grip.global_transform = receiver_to_grip.global_transform * grip_to_receiver.transform.affine_inverse()
+	var stock_to_receiver = stock.find_child("Receiver_Attach")
+	var receiver_to_stock = receiver.find_child("Stock_Attach")
+	stock.global_transform = receiver_to_stock.global_transform * stock_to_receiver.transform.affine_inverse()
+
 	# Recalculate stats
 	#ItemSlot Based implementation
 	var build_slot = (inventory.get_ui_slot(5) as DraggableItem)
 	build_slot.visible = true
-	(inventory.get_ui_slot(5) as DraggableItem).item = receiver	
+	(inventory.get_ui_slot(5) as DraggableItem).item = receiver
 	receiver.position = Vector3(0,0,0)
 	receiver.rotation = rotation
-	(inventory.get_ui_slot(5) as DraggableItem).regenerate_icon(true)
-	(inventory.get_ui_slot(0) as DraggableItem).item = null
-	(inventory.get_ui_slot(0) as DraggableItem).regenerate_icon()
-	(inventory.get_ui_slot(1) as DraggableItem).item = null
-	(inventory.get_ui_slot(1) as DraggableItem).regenerate_icon()
-	(inventory.get_ui_slot(2) as DraggableItem).item = null
-	(inventory.get_ui_slot(2) as DraggableItem).regenerate_icon()
+	for icon: DraggableItem in inventory.get_slots():
+		if icon != (inventory.get_ui_slot(5) as DraggableItem):
+			icon.item = null
+		icon.regenerate_icon(true)
+	#Physical item positions, todo
+	#grip.global_position = (get_node("GripPosition") as Marker3D).global_position
+	#grip.rotation = (get_node("GripPosition") as Marker3D).rotation
+	#receiver.global_position = (get_node("ReceiverPosition") as Marker3D).global_position
+	#receiver.rotation = (get_node("ReceiverPosition") as Marker3D).rotation
+	#barrel.global_position = (get_node("BarrelPosition") as Marker3D).global_position
+	#barrel.rotation = (get_node("BarrelPosition") as Marker3D).rotation
