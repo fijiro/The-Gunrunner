c9db169e4adf70c79c63b4d7df3f3fb25ceeb173
fijiro <jiro.protonmail@protonmail.com>
2025-04-21 23:16:18 +0300

    Cleanup prints

--- scripts/ui/inventory/inventory_base.gd
+++ scripts/ui/inventory/inventory_base.gd
@@ -4,8 +4,8 @@ class_name InventoryBase extends Node3D
 @export var ui_scene: PackedScene
 var ui: CanvasLayer
 var items: Array[Node3D] = []
-# TODO: When ui is created: 
-# 1. Generate inventory (3D nodes) and set them invisible ✔
+# When inventory is created: 
+# 1. Generate UI, items (3D nodes) and set them invisible ✔
 # 	1.1 Dummy values for now ✔
 # 2. Assign 3D_inventory to ui item slots ✔
 # 	2.1 generate a pointer to the ui ✔
@@ -18,11 +18,10 @@ func _ready() -> void:
 	for item_slot: TextureRect in ui.get_node("ItemSlots").get_children():
 		item_slot.setup(null, self)
 	#Add existing items in inventory
-	#add_item(dummy_scene.instantiate())
 	for child in get_children():
 		add_item(child)
-		pass
-
+		
+## Returns was item successfully added to inventory.
 func add_item(item: Node3D) -> bool:
 	if item == null: return false
 	items.append(item)
@@ -30,16 +29,16 @@ func add_item(item: Node3D) -> bool:
 	else: add_child(item)
 	var item_slot: DraggableItem = get_ui_slot(items.find(item))
 	item_slot.setup(item, self)
-	item.visible = true
+	item.visible = false
 	return true
 
-## Returns null or TextureRect slot for item.
+## Returns null or DraggableItem slot for index.
 func get_ui_slot(index: int) -> DraggableItem:
 	return ui.get_node("ItemSlots").get_child(index) if index > -1 else null
 	
+## Returns all inventory slots.
 func get_slots() -> Array[DraggableItem]:
 	var slots: Array[DraggableItem] = []
 	for item in ui.get_node("ItemSlots").get_children():
-		if item is DraggableItem: slots.append(item as DraggableItem)
-		else: push_warning("Item is not a DraggableItem!")
+		if item is DraggableItem: slots.append(item)
 	return slots
