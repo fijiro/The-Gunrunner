64ccf7778ca2f29056e87eef2cd6446631e7a62a
fijiro <jiro.protonmail@protonmail.com>
2025-04-27 13:23:20 +0300

    Cartridge Assembly

--- scripts/world/crafting/assembler.gd
+++ scripts/world/crafting/assembler.gd
@@ -14,7 +14,7 @@ func assemble() -> void:
 		return
 	var items: Dictionary = inventory.get_items()
 	print(items)
-	var product := product_scene.instantiate()
+	var product: CartridgePart = product_scene.instantiate()
 	var case: CartridgePart = items.get("case")
 	if items.size() < 3: return
 	else: print("BUILD POSSIBLE")
@@ -26,17 +26,28 @@ func assemble() -> void:
 		part.visible = true
 		part.reparent(product)
 		if part_name == "case": continue
-		var case_to_part = case.find_child("%s_Attach" % part_name.capitalize())
-		var part_to_case = part.find_child("Case_Attach")
+		var case_to_part: Marker3D = case.find_child("%s_Attach" % part_name.capitalize())
+		var part_to_case: Marker3D = part.find_child("Case_Attach")
 		if case_to_part and part_to_case:
 			part.global_transform = case_to_part.global_transform * part_to_case.transform.affine_inverse()
+			part.rotation += case_to_part.rotation
 		else: print("ERROR: NO ATTACH POINT FOUND")
 		# Recalculate stats
 		product.price += part.price
 		product.weight += part.weight
 		product.accuracy *= part.accuracy
 		#product.ergo *= part.ergo
-
+		
+	product.position = $ProductPosition.position
+	product.rotation = $ProductPosition.rotation
+	product.scale = Vector3(10,10,10)
+	product.visible = true
+	for slot: DraggableItem in inventory.get_slots():
+		if slot != product_slot:
+			slot.item = null
+			#slot.visible = false
+		slot.regenerate_icon(true)
+	
 func _get_product_slot() -> DraggableItem:
 	return inventory.ui.get_node("ProductSlot")
 	
