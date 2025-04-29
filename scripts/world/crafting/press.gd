55b09c163cfd8ceb9f18a97a30a6d8a4c41490a5
fijiro <jiro.protonmail@protonmail.com>
2025-04-29 18:42:59 +0300

    Production works until product reaches max stack size

--- scripts/world/crafting/press.gd
+++ scripts/world/crafting/press.gd
@@ -1,22 +1,24 @@
 extends InteractableObject
 @export var product_scene: PackedScene
-var finish_slot: ItemSlot
+var product_slot: ItemSlot
 var coil_slot: ItemSlot
 func _ready() -> void:
 	super._ready()
 	inventory.ui.connect("start_press", Callable(self, "press"))
-	#Render Coil and setup finish_slot
+	#Render Coil and setup product_slot
 	coil_slot = inventory.get_ui_slot("CoilSlot")
 	coil_slot.dropped_data.connect(_coil_slot_update)
-	finish_slot = _get_finish_slot()
-	finish_slot.setup(null,inventory)
+	product_slot = _get_product_slot()
+	product_slot.setup(null, inventory)
 
 func press() -> void:
-	if not coil_slot.item or finish_slot.item: return
-	var product := product_scene.instantiate()
-	inventory.add_item(product, finish_slot)
+	if not coil_slot.item: return
+	if !product_slot.stack_size:
+		var product := product_scene.instantiate()
+		inventory.add_item(product, product_slot)
+	else: product_slot.add_one()
 
-func _get_finish_slot() -> ItemSlot:
+func _get_product_slot() -> ItemSlot:
 	return inventory.ui.get_node("FinishSlot")
 	
 func _coil_slot_update():
