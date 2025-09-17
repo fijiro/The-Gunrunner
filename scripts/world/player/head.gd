c38edfd5c61078e85b9bf8c9ee10bb0cbee61a4b
fijiro <jiro.protonmail@protonmail.com>
2025-09-17 12:51:37 +0300

    Can equip other items than Weapons

--- scripts/world/player/head.gd
+++ scripts/world/player/head.gd
@@ -6,18 +6,22 @@ var weapon: Weapon
 
 ## (Un)equips the item in hand and resets existing signal connections.
 func equip_item() -> void:
+	# Unequip old item
 	if weapon:
 		weapon.weapon_fired.disconnect(hand.recoil_weapon)
 		weapon.weapon_fired.disconnect(player_inv.adjust_ammo)
 		weapon = null
 	if hand.get_child_count() < 3: return
 	var item = hand.get_child(2)
-	if hand.get_child(2).is_class("Weapon"): print("is weapon")
-	print(item.get_class())
-	weapon = hand.get_child(2)
-	hand.weapon = weapon
-	weapon.weapon_fired.connect(hand.recoil_weapon)
-	weapon.weapon_fired.connect(player_inv.adjust_ammo)
+	# Equip new item
+	if item is Weapon: 
+		print("is weapon")
+		weapon = hand.get_child(2)
+		hand.weapon = weapon
+		weapon.weapon_fired.connect(hand.recoil_weapon)
+		weapon.weapon_fired.connect(player_inv.adjust_ammo)
+	if item.is_class("RigidBody3D"):
+		(item as RigidBody3D).freeze = true
 
 # Handle input here so unequipped weapons don't care about input
 func fire_input():
