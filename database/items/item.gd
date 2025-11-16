8199b6e5e49337014779a423896d85a6d35332ba
fijiro <jiro.protonmail@protonmail.com>
2025-11-16 04:09:19 +0200

    Change price to int

--- database/items/item.gd
+++ database/items/item.gd
@@ -3,6 +3,6 @@ class_name Item extends Node3D
 @export var part := "Item"
 @export var desc := "Default item."
 @export var type := "item"
-@export var price := 100.0
+@export var price := 100
 @export var weight := 1.0
 @export var max_stack := 1
