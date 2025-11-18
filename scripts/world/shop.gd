4ac09d7efb7e957e1488377d3ed18b7f88a9d821
fijiro <jiro.protonmail@protonmail.com>
2025-11-18 15:48:26 +0200

    Cleanup

--- scripts/world/shop.gd
+++ scripts/world/shop.gd
@@ -4,13 +4,9 @@ extends InteractableObject
 func _ready():
 	super._ready()
 	inventory.ui.connect("buy_pressed", _buy_shipment)
-# TODO:
-# Select part and amount
-# Buy shipment action
-# Deliver shipment
 func _buy_shipment() -> void:
 	var price := int((inventory.ui.get_node("PriceLabel") as Label).text)
-	# if price > player.inventory.money: return
+	if not price or price > player.inventory.money: return
 	player.inventory.adjust_money(-price)
 	_deliver_shipment()
 	_clear_shop()
